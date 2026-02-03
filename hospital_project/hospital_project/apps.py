84af1e535c2b33fb3c20459392f9877fe2fbb9ed
mayank <mayankkishanmathur@gmail.com>
2026-02-03 14:47:24 +0530

    Flatten repo structure - move hospital_project files to root

--- hospital_project/hospital_project/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class HospitalProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospital_project'
-    
-    def ready(self):
-        # Import admin configuration when app is ready
-        import admin_configuration
--- hospital_project/hospital_project/asgi.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HospitalProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospital_project'
